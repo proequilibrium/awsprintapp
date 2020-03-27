ea1b86b975e6e5b33694bddaac8ef6a1f36da5c5
proequilibrium <proequilibrium@gmail.com>
2020-03-27 16:28:20 +0100

    added MyLayout Signing Profile without amplify

--- src/boot/amplify.js
+++ src/boot/amplify.js
@@ -1,31 +1,31 @@
-import Amplify, * as AmplifyModules from "aws-amplify";
-import { AmplifyPlugin, AmplifyEventBus } from "aws-amplify-vue";
-import AwsExports from "../aws-exports";
-Amplify.configure(AwsExports);
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin, AmplifyEventBus } from 'aws-amplify-vue'
+import AwsExports from '../aws-exports'
+Amplify.configure(AwsExports)
 
 export default async ({ router, Vue }) => {
-  Vue.use(AmplifyPlugin, AmplifyModules);
-  Vue.prototype.$Amplify = Amplify;
-  Vue.prototype.$AmplifyEventBus = AmplifyEventBus;
-  Vue.prototype.$Auth = AmplifyModules.Auth;
+  Vue.use(AmplifyPlugin, AmplifyModules)
+  Vue.prototype.$Amplify = Amplify
+  Vue.prototype.$AmplifyEventBus = AmplifyEventBus
+  Vue.prototype.$Auth = AmplifyModules.Auth
 
   router.beforeResolve((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
       // eslint-disable-next-line no-unused-vars
-      let user;
+      let user
       Vue.prototype.$Amplify.Auth.currentAuthenticatedUser()
         .then(data => {
           if (data && data.signInUserSession) {
-            user = data;
+            user = data
           }
-          next();
+          next()
         })
         .catch(e => {
           next({
-            path: "/auth"
-          });
-        });
+            path: '/auth'
+          })
+        })
     }
-    next();
-  });
-};
+    next()
+  })
+}
